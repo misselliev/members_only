02e2ec852b0738f16ee45cff65f79cf48a7c6ae4
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-18 17:52:11 -0700

    adding remember method to helpers

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookied.permanent[:remember_token] = user.remember_token
   end
 end
