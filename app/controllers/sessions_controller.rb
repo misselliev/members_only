40b284b927b3200a08f98e049df716ae144e52e6
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-23 16:50:01 -0700

    Styling views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      flash[:success] = "Welcome!"
+      flash[:success] = "Successfully logged in!"
       log_in @user
       redirect_to new_post_path
     else
