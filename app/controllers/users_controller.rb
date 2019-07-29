7bc23ae5394af9151a2715d7198321caf18f6bae
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-07-29 14:38:53 -0700

    fixing error in signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,20 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-def create
-    @user = User.create(user_params)
-    if @user.save
-      log_in @user
-      flash[:info] = 'User Created'
-      redirect_to root_path
-    else
-      render 'new'
-    end
+  def new
+    @user = User.new
   end
+
+  def create
+      @user = User.create(user_params)
+      if @user.save
+        log_in @user
+        flash[:info] = 'User Created'
+        redirect_to root_path
+      else
+        render 'new'
+      end
+    end
 
   private
 
